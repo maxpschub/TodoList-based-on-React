e5a285a6501b56624a5a056d692c06488f3dc801
PSC <majiapsc@126.com>
2017-01-10 16:29:08 +0800

    review

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
 	entry: __dirname + "/app/main.js",
 	output: {
 		path: __dirname + "/public",
-		publicPath: "/assets/",
+		publicPath: "/public",
 		filename: "bundle.js"
 	},
 	module: {
